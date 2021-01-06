64a4209b840959ecf74cb210a984453a9bbfee41
Quan Phan <hoangquanphan2906@gmail.com>
2021-01-06 10:05:08 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import katex from "katex";
+import "katex/dist/katex.min.css";
+// import MathQuill from "./math-quill-loader";
+import React, { createRef, useEffect } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import mathquill4quill from "mathquill4quill";
+import "mathquill4quill/mathquill4quill.css";
+import "react-quill/dist/quill.snow.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const reactQuill = createRef();
+
+    useEffect(() => {
+        const enableMathQuillFormulaAuthoring = mathquill4quill({
+            Quill,
+            katex,
+            // MathQuill,
+        });
+        enableMathQuillFormulaAuthoring(reactQuill.current.editor);
+    });
+
+    const printToConsole = () => {
+        console.log(reactQuill.current.state.value);
+    };
+
+    return (
+        <div className="App">
+            <ReactQuill
+                theme="snow"
+                ref={reactQuill}
+                modules={{
+                    formula: true,
+                    toolbar: [["formula"]],
+                }}
+            />
+            <button onClick={printToConsole}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/math-quill-loader.js
@@ -0,0 +1,4 @@
+const MathQuill = require("@edtr-io/mathquill/build/mathquill.js");
+const MQ = MathQuill.getInterface(2);
+
+export default MQ;
